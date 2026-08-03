ac0668750571bb0aa77c904f5c88ca492d2668f9
miktok9daa <miktok9daa@users.noreply.github.com>
2026-08-03 16:41:49 +0530

    fix: bundle and install Noto Sans KR font so Korean subtitles render correctly

--- main.py
+++ main.py
@@ -322,7 +322,7 @@ LastStyleStorage: Default
 
 [V4+ Styles]
 Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
-Style: Default,Noto Sans CJK KR,160,&H00FFFFFF,&H000000FF,&H00000000,&H80000000,1,0,0,0,100,100,0,0,1,6,3,5,10,10,0,1
+Style: Default,Noto Sans KR,160,&H00FFFFFF,&H000000FF,&H00000000,&H80000000,1,0,0,0,100,100,0,0,1,6,3,5,10,10,0,1
 
 [Events]
 Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
